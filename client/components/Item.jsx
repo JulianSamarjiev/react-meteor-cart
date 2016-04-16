8b6753d6525f1f418c1458b4283f35164af52909
JulianSamarjiev <juliansamarjiev@gmail.com>
2016-04-16 21:48:25 +0200

    add delete functionality from a mongo collection

--- client/components/Item.jsx
+++ client/components/Item.jsx
@@ -1,10 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Items } from '../../api/items.js';
+
 export default class Item extends Component {
+  deleteThisItem() {
+    Items.remove(this.props.item._id);
+  }
   render() {
     return (
       <div>
-        {this.props.item.text}
+        <span>{this.props.item.text}</span>
+        <span onClick={this.deleteThisItem.bind(this)}>X</span>
       </div>
     );
   }
